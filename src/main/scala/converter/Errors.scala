93af9f13902544c7ab70a33992b61c81863784a7
Karpuhin Grigoriy <368279@niuitmo.ru>
2023-12-10 23:46:37 +0300

    Delete src directory

--- src/main/scala/converter/Errors.scala
+++
@@ -1,7 +0,0 @@
-package converter
-
-class MoneyAmountShouldBePositiveException extends Exception
-
-class UnsupportedCurrencyException extends Exception
-
-class WrongCurrencyException extends Exception
